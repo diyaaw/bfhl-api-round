57d7d740e940eb5761b7e4bf7d696f517a150f12
Diya Wadhwa <diya.wadhwa24@gmail.com>
2026-05-26 14:16:54 +0530

    fix: switch to static export for reliable Netlify direct deploy

---
+++ out/_next/static/BNWpWizdh1FpbkF2qgNtR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/b7f037f39f4246a5.css","static/chunks/pages/index-cea4a9605c6503ac.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
